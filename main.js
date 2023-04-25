7335e7467680ef6296e1fb8384c4a9e7146ff25f
zsoolti8917 <zsolt.varju.rl@gmail.com>
2023-04-25 17:13:24 +0200

    Solved problem with share button

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 const btn = document.querySelector(".content__author-share");
 
 btn.addEventListener("click", function(e){
-const shareView = document.querySelector(".content__author-view-share");
-shareView.style.display = "flex";
-const author = document.querySelector(".content__author");
-author.style.display = "none";
+    const shareView = document.querySelector(".content__author-view-share");
+    const author = document.querySelector(".content__author");
+    var width = window.innerWidth;
+    console.log(width);
+    if( width > 768 ) {
+        if (shareView.style.display === "none") {
+            shareView.style.display = "flex";
+          } else {
+            shareView.style.display = "none";
+          }
+        
+    } else {
+
+    shareView.style.display = "flex";
+
+    author.style.display = "none";
+    }
+
 })
 
 const btn2 = document.querySelector(".content__author-share2");
